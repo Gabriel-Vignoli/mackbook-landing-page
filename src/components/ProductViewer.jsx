b012f6084510cce1e4684207b5d5b495fc67f02b
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-05-28 20:45:48 -0300

    feat: add macbook 3d model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,7 +1,8 @@
 import clsx from "clsx";
 import useMacbookStore from "..";
 import { Canvas } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -57,11 +58,16 @@ const ProductViewer = () => {
         </div>
       </div>
 
-              <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}></Box>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <ambientLight intensity={1}></ambientLight>
 
-                <OrbitControls enableZoom={false}></OrbitControls>
-              </Canvas>
+        <MacbookModel14 scale={0.06} position={[0, 0, 0]}></MacbookModel14>
+
+        <OrbitControls enableZoom={false}></OrbitControls>
+      </Canvas>
     </section>
   );
 };
