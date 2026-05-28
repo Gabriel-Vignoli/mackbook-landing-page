6175cdc0f6567324f913152f80126b02b56870a8
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-05-28 20:28:06 -0300

    feat: add Product Viewer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
     return (
         <main>
             <NavBar></NavBar>
             <Hero></Hero>
+            <ProductViewer></ProductViewer>
         </main>
     )
 }
---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,69 @@
+import clsx from "clsx";
+import useMacbookStore from "..";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+
+const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useMacbookStore();
+
+  return (
+    <section id="product-viewer">
+      <h2>Veja mais de perto</h2>
+      <div className="controls">
+        <p className="info">
+          MacBook Pro {scale} em {color}
+        </p>
+
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              className={clsx(
+                "bg-neutral-300",
+                color === "#abd5bd" && "active",
+              )}
+              onClick={() => setColor("#abd5bd")}
+            ></div>
+            <div
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active",
+              )}
+              onClick={() => setColor("#2e2c2e")}
+            ></div>
+          </div>
+
+          <div className="size-control">
+            <div
+              className={clsx(
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white",
+              )}
+              onClick={() => setScale(0.06)}
+            >
+              <p>14</p>
+            </div>
+            <div
+              className={clsx(
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white",
+              )}
+              onClick={() => setScale(0.08)}
+            >
+              <p>16</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+              <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}></Box>
+
+                <OrbitControls enableZoom={false}></OrbitControls>
+              </Canvas>
+    </section>
+  );
+};
+
+export default ProductViewer;
